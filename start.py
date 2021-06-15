b39d00b7dc2ce9b99743e804f3f6909335d573b5
clément-devevey <68287089+Clement-Devevey@users.noreply.github.com>
2021-06-15 09:23:38 +0200

    Delete start.py

--- start.py
+++
@@ -1,8 +0,0 @@
-import sys,subprocess
-
-
-musique_boucle = subprocess.Popen(["while true; do aplay /game/Resources/musiques/8bit.wav -N --test-nowait ; sleep 2; done"], shell = True)
-subprocess.Popen(["amixer cset numid=1 70%"],shell=True)
-musique_boucle.terminate()
-subprocess.Popen(['kill -9 $(ps aux | grep "aplay /game/Resources/musiques/8bit.wav -N --test-nowait" | grep -v "grep" |tr -s " "| cut -d " " -f 2)'],  shell=True )
-sys.exit()
