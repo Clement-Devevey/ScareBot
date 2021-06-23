530cf89be18ae6a8d028156d5cc4286fad68eb9d
clément-devevey <68287089+Clement-Devevey@users.noreply.github.com>
2021-06-23 12:07:13 +0200

    Add files via upload

---
+++ rootfs_overlay/etc/gif.sh
@@ -0,0 +1,10 @@
+while true
+do
+ fbv -s 1 /game/Resources/images/gif/frame1.gif
+ sleep 1
+ fbv -s 1 /game/Resources/images/gif/frame2.gif
+ sleep 1
+ fbv -s 1 /game/Resources/images/gif/frame3.gif
+ sleep 1
+done
+
---
+++ rootfs_overlay/game/remise_score_zero.py
@@ -0,0 +1,3 @@
+f_high_score = open(r"./Resources/high_score.txt", "w") # Ouverture en écriture.
+f_high_score.write(str(0))
+f_high_score.close()
