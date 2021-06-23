ebafc30d92265dfb8cdfea49fe47cfc625632dbb
Humbert Alexandre <alex.hum@hotmail.fr>
2021-06-23 22:59:07 +0200

    Update post-build.sh

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -2,13 +2,10 @@
 
 set -u
 set -e
+chmod 777 ${TARGET_DIR}/etc/init.d/S00gif
+chmod 777 ${TARGET_DIR}/etc/init.d/S01game
 rm -f ${TARGET_DIR}/etc/init.d/S01syslogd
 rm -f ${TARGET_DIR}/etc/init.d/S02klogd
 rm -f ${TARGET_DIR}/etc/init.d/S02sysctl
 rm -f ${TARGET_DIR}/etc/init.d/S20urandom
-# Add a console on tty1
-if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
-	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
-fi
+rm -f ${TARGET_DIR}/etc/init.d/S40network
